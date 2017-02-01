95dee51499cbf2b1b73bdbbb9c91087810ccb7b2
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-02-01 18:39:14 +0100

    Responsive slider produits

--- scripts/app.js
+++ scripts/app.js
@@ -22,9 +22,25 @@ $(document).ready(function(){
     autoplay : true,
     autoplaySpeed : 2500,
     dots : true,
-    arrows:true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   });
 });
 
