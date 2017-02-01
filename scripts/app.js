3e4d505726909b64f8001d8b377754cf4d56ff2f
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-02-01 14:32:58 +0100

    Navbar large

--- scripts/app.js
+++ scripts/app.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
     dots : true
   });
   $('.sliderProduits').slick({
-    autoplay : false,
+    autoplay : true,
     autoplaySpeed : 2500,
     dots : true,
     arrows:true,
