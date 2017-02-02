45043e537fa8900e6ebc0c47e93ade366469ce46
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-02-02 17:55:03 +0100

    Fix ?

--- scripts/app.js
+++ scripts/app.js
@@ -19,7 +19,7 @@ function checkReponse(i, reponseQuestion) {
   $('#imageQuizz'+i).toggleClass('cache');
   $('.boutonQuizz'+i).toggleClass('cache');
   if (bonnesReponses < 3) {
-    $('#resultatQuizz > p)'.text(bonnesReponses" bonnes réponses... Vous n'êtes pas encore un <span class='hashtag'>#CocoAddict</span>");
+    $('#resultatQuizz > p)'.text(bonnesReponses + " bonnes réponses... Vous n'êtes pas encore un <span class='hashtag'>#CocoAddict</span>");
   }
 }
 
